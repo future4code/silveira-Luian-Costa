9ead6a8646de7ee4e727fe29baac6be65bdd7e5a
unknown <luiangcosta>
2022-04-11 04:47:39 -0300

    Removido Botao de Detalhes-Playlist pois estava crashando o app.js

--- modulo2/projeto-labefy/projeto-labefy/src/App.js
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import TelaPrincipal from './components/TelaPrincipal';
 import CriarPlaylists from './components/CriarPlaylist';
 import TelaPlaylists from './components/TelaPlaylists';
-import DetalhesPlaylist from './components/DetalhesPlaylist';
 
 export default class App extends React.Component {
   state = {
     telaAtual: "telaPrincipal",
-    idPlaylist: "",
-    namePlaylist: "",
   }
 
   escolheTela = () => {
@@ -19,8 +16,6 @@ export default class App extends React.Component {
         return <TelaPlaylists irParaHome={this.irParaHome} irParaCriarPlaylists={this.irParaCriarPlaylists} />
       case "criarPlaylists":
         return <CriarPlaylists irParaHome={this.irParaHome} irParaTelaPlaylists={this.irParaTelaPlaylists} />
-      case "detalhes":
-        return <DetalhesPlaylist idPlayList={this.state.idPlayList} namePlaylist={this.state.namePlaylist} irParaHome={this.irParaHome} />
       default:
         return <>Erro! Página não encontrada:(</>
     }
@@ -37,10 +32,6 @@ export default class App extends React.Component {
   irParaHome = () => {
     this.setState({ telaAtual: "telaPrincipal" })
   }
-  irParaDetalhes = (id, name) => {
-    this.setState({ telaAtual: "detalhes", idPlayList: id, namePlaylist: name })
-  }
-
 
   render() {
     return (
