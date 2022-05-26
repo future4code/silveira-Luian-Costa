e2941c39a9ac09f36147b281e63b55876042785c
luiangcosta <luiangcosta@outlook.com>
2022-05-26 02:17:31 -0300

    Correções

--- modulo4/intro-typescript/src/index.ts
+++ modulo4/intro-typescript/src/index.ts
@@ -1,51 +1,51 @@
-//Exercício 1
+// Exercício 1
 
-// function checaTriangulo(a: number, b: number, c:number ) :string {
-//     if(a !== b && b !== c){
-//         return "Escaleno"
-//     }else if(a === b && b === c){
-//         return "Equilátero"
-//     }else{
-//         return "Isósceles"
-//     }
-// }
-// console.log(checaTriangulo(4, 5, 2))
+function checaTriangulo(a: number, b: number, c:number ) :string {
+    if(a !== b && b !== c){
+        return "Escaleno"
+    }else if(a === b && b === c){
+        return "Equilátero"
+    }else{
+        return "Isósceles"
+    }
+}
+console.log(checaTriangulo(4, 5, 2))
 
-// //Exercício 2
+//Exercício 2
 
-// function imprimeTresCoresFavoritas(): Array<string> {
-//     const cor1: string = process.argv[2]
-//     const cor2: string = process.argv[3]
-//     const cor3: string = process.argv[4]
-//     return [cor1, cor2, cor3]
-// }
-// console.log(imprimeTresCoresFavoritas())
+function imprimeTresCoresFavoritas(): Array<string> {
+    const cor1: string = process.argv[2]
+    const cor2: string = process.argv[3]
+    const cor3: string = process.argv[4]
+    return [cor1, cor2, cor3]
+}
+console.log(imprimeTresCoresFavoritas())
 
-// //Exercício 3
+//Exercício 3
 
-// function checaAnoBissexto(ano: number): boolean {
-//     const cond1 = ano % 400 === 0
-//     const cond2 = (ano % 4 === 0) && (ano % 100 !== 0)
-//     return cond1 || cond2
-// }
-// console.log(checaAnoBissexto(2022))
+function checaAnoBissexto(ano: number): boolean {
+    const cond1 = ano % 400 === 0
+    const cond2 = (ano % 4 === 0) && (ano % 100 !== 0)
+    return cond1 || cond2
+}
+console.log(checaAnoBissexto(2022))
 
-// //Exercício 4
+//Exercício 4
 
-// function comparaDoisNumeros(num1: number, num2: number): number {
-//     let maiorNumero;
-//     let menorNumero;
+function comparaDoisNumeros(num1: number, num2: number): number {
+    let maiorNumero;
+    let menorNumero;
 
-//     if(num1 > num2){
-//         maiorNumero = num1;
-//         menorNumero = num2
-//     }else{
-//         maiorNumero = num2;
-//         menorNumero= num1
-//     }
-//     return maiorNumero - menorNumero
-// }
-// console.log(comparaDoisNumeros(6, 10))
+    if(num1 > num2){
+        maiorNumero = num1;
+        menorNumero = num2
+    }else{
+        maiorNumero = num2;
+        menorNumero= num1
+    }
+    return maiorNumero - menorNumero
+}
+console.log(comparaDoisNumeros(6, 10))
 
 //Exercício 5
 
