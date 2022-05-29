c82f05ee08632bb117a20d34e05455ebda0b5bac
luiangcosta <luiangcosta@outlook.com>
2022-05-29 19:18:23 -0300

    exercicio3

--- modulo4/lista-typescript/src/exercicio3.ts
+++ modulo4/lista-typescript/src/exercicio3.ts
@@ -1,14 +1,35 @@
 enum GENERO {
-    ACAO = "ação",
-    DRAMA = "drama",
-    COMEDIA = "comédia",
-    ROMANCE = "romance",
-    TERROR = "terror"
+    ACAO = "Ação",
+    DRAMA = "Drama",
+    COMEDIA = "Comédia",
+    ROMANCE = "Romance",
+    TERROR = "Terror"
 }
 
-type informacao = {
-    nome: string,
+type Informacao = {
+    nomeDoFilme: string,
     anoDeLancamento: number,
-    genero: GENERO,
-    pontuacao?: number
+    generoDoFilme: GENERO,
+    pontuacaoDoFilme?: number
 }
+
+function retornaInformacao(nome: string, ano: number, genero: GENERO, pontuacao?: number): Informacao {
+    if (!pontuacao) {
+        const filme = {
+            nomeDoFilme: nome,
+            anoDeLancamento: ano,
+            generoDoFilme: genero,
+        }
+        return filme;
+    } else if (nome && ano && genero && pontuacao) {
+        const filme = {
+            nomeDoFilme: nome,
+            anoDeLancamento: ano,
+            generoDoFilme: genero,
+            pontuacaoDoFilme: pontuacao
+        }
+        return filme
+    }
+}
+console.log(retornaInformacao("Garfield 2", 2006, GENERO.COMEDIA))
+console.log(retornaInformacao("Duro de Matar", 1988, GENERO.ACAO, 8.2))
