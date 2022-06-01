7770d04d3c4e1da413c4764d409ee58a0b99f4ff
luiangcosta <luiangcosta@outlook.com>
2022-06-01 04:14:13 -0300

    Exercícios finalizados + documentação da api

--- modulo4/aprofundamento-express/src/index.ts
+++ modulo4/aprofundamento-express/src/index.ts
@@ -38,3 +38,45 @@ app.post("/criar-afazer", (req, res) => {
     arrayDeAfazeres.push(novoAfazer)
     res.status(200).send(arrayDeAfazeres)
 })
+
+//Exercício 6
+
+app.put("/edita-afazer/:id", (req, res) => {
+    const afazerId = Number(req.params.id)
+
+    const editaAfazer = arrayDeAfazeres.filter((afazer) => {
+        return afazer.id === afazerId
+    }).map((afazer) => {
+        return afazer = {
+            userId: afazer.id,
+            id: afazer.id,
+            title: afazer.title,
+            completed: !afazer.completed
+        }
+    })
+    res.status(200).send(editaAfazer)
+})
+
+//Exercício 7
+
+app.delete("/deleteafazer/:id", (req, res) => {
+    const afazerId = Number(req.params.id)
+
+    const deletarAfazer = arrayDeAfazeres.filter((afazer) => {
+        return afazer.id !== afazerId
+    })
+    res.status(200).send(deletarAfazer)
+})
+
+//Exercício 8
+
+app.get("/afazer/user/:id", (req, res) => {
+    const userId = Number(req.params.id)
+
+    const afazerUsuario = arrayDeAfazeres.filter((afazer) => {
+        return userId === afazer.userId
+    })
+    res.status(200).send(afazerUsuario)
+})
+
+//Exercício 9 -> https://documenter.getpostman.com/view/20353668/Uz5DrdRF
