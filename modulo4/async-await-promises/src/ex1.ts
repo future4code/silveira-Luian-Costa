aab0d26ad40bc79ae186a0d3ff3ac19fd5b10414
luiangcosta <luiangcosta@outlook.com>
2022-06-14 15:55:54 -0300

    ex1

--- modulo4/async-await-promises/src/ex1.ts
+++ modulo4/async-await-promises/src/ex1.ts
@@ -5,3 +5,14 @@ async function getSubscribers(): Promise<any[]> {
     const response = await axios.get(`${BASE_URL}/subscribers`);
     return response.data;
 };
+
+const main = () => {
+    try {
+        getSubscribers()
+            .then(console.log)
+    } catch (error: any) {
+        const response = error.response?.data || error.message
+        console.log(response)
+    }
+}
+main()
