7a1c601a3a039efe699b48fca2c01bfe27d6edd6
luiangcosta <luiangcosta@outlook.com>
2022-07-08 22:15:43 -0300

    iwfs-labook

---
+++ modulo5/iwfs-labook/src/services/Authenticator.ts
@@ -0,0 +1,26 @@
+import * as jwt from "jsonwebtoken"
+import { AuthenticationData } from "../types/AuthenticationType"
+
+export class Authenticator {
+    public generateToken(payload: AuthenticationData): string {
+        const token = jwt.sign(
+            {
+                id: payload.id
+            },
+            process.env.JWT_KEY as string,
+            {
+                expiresIn: "50m"
+            }
+        )
+        return token
+    }
+
+    public getData(token: string): AuthenticationData {
+        const payload = jwt.verify(token, process.env.JWT_KEY as string) as any
+        const result = {
+            id: payload.idP
+        }
+        return result
+    }
+
+}
